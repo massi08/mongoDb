2a29e88f70545eac481323ec4a396cd9d5632d16
Massi <m.bouneffa@upfit-services.com>
2016-11-30 15:23:48 +0100

    Start mif04 mongoDb

---
+++ MIF04-GDW-TPMongoDB-Ex1.js
@@ -0,0 +1,59 @@
+
+/*** EXO 1 : Q1 **************************************************************/
+
+var nom_etu1 = "Bouneffa Massinissa";
+var num_etu1 = "p1310939";
+
+var nom_etu2 = "Abebe Fanuel";
+var num_etu2 = "p1311388";
+
+
+/*** EXO 1 : Q2 **************************************************************/
+
+var exo1q2map = function () {
+  /*TODO*/
+}
+
+var exo1q2red = function (key, values) {
+  /*TODO*/
+}
+
+// db.restaurants.mapReduce(exo1q2map, exo1q2red, {out : {inline:1}});
+// reponse : 25359
+
+/*** EXO 1 : Q3 **************************************************************/
+
+var exo1q3map = function () {
+  /*TODO*/
+}
+
+var exo1q3red = exo1q2red;
+
+// reponse : 93463
+
+
+/*** EXO 1 : Q4 **************************************************************/
+
+var exo1q4map = function () {
+  /*TODO*/
+}
+
+var exo1q4red = exo1q2red;
+
+// reponse : 6086
+
+/*** EXO 1 : Q5 **************************************************************/
+
+var exo1q5 = {}   /*TODO*/;
+// reponse : 6086
+
+/*** EXO 1 : Q6 **************************************************************/
+
+var exo1q6map = function () {
+  /*TODO*/
+}
+
+var exo1q6red = exo1q2red;
+// reponse : 6
+
+
---
+++ MIF04-GDW-TPMongoDB-Ex2.js
@@ -0,0 +1,245 @@
+/*** EXO 2 : Q1 **************************************************************/
+
+var exo2q1map = function () {
+  /*TODO*/
+}
+
+var exo2q1red = function (key, values) {
+  /*TODO*/
+}
+
+// 85 réponses
+//{
+//	"results" : [
+//		{
+//			"_id" : "American ",
+//			"value" : 0
+//		},
+//		{
+//			"_id" : "Bakery",
+//			"value" : 0
+//		},
+//		...
+
+/*** EXO 2 : Q2 **************************************************************/
+
+var exo2q2map = function () {
+  /*TODO*/
+}
+
+var exo2q2red = function (key, values) {
+  /*TODO*/
+}
+
+//réponses
+//{
+//	"results" : [
+//		{
+//			"_id" : "Bronx",
+//			"value" : {
+//				"restaurants" : [
+//					"Morris Park Bake Shop",
+//					"E & L Bakery & Coffee Shop",
+//					...
+//				]
+//			}
+//		},
+//		{
+//			"_id" : "Brooklyn",
+//			"value" : {
+//				"restaurants" : [
+//					"Tropical House Baking Co.",
+//					"Cousin John'S Cafe And Bakery",
+//					...
+//				]
+//			}
+//		},
+//    ...
+
+
+/*** EXO 2 : Q3 **************************************************************/
+
+var exo2q3map = function () {
+  /*TODO*/
+};
+
+var exo2q3red = function (key, values) {
+  /*TODO*/
+}
+
+//réponses
+//{
+//	"results" : [
+//		{
+//			"_id" : "Bronx",
+//			"value" : {
+//				"Bakery" : 0,
+//				"American " : 0,
+//				"Ice Cream, Gelato, Yogurt, Ices" : 0,
+//				"Chinese" : 0,
+//				...
+//			}
+//		},
+//		{
+//			"_id" : "Brooklyn",
+//			"value" : {
+//				"Hamburgers" : 0,
+//				...
+
+/*** EXO 2 : Q4 **************************************************************/
+
+
+exo2q4map = function () {
+  /*TODO*/
+}
+
+exo2q4red = function (key, values) {
+  /*TODO*/
+}
+
+exo2q4fin = function (key, val) {
+  /*TODO*/
+};
+
+//{
+//	"results" : [
+//		{
+//			"_id" : "Bronx",
+//			"value" : {
+//				"w" : 8706,
+//				"s" : 96070,
+//				"avg" : 11.034918447048012
+//			}
+//		},
+//		{
+//			"_id" : "Brooklyn",
+//			"value" : {
+//				"w" : 21963,
+//				"s" : 251409,
+//				"avg" : 11.44693347903292
+//			}
+//		},
+//    ...
+
+
+// var exo2q4 = db.restaurants.mapReduce(exo2q4map, exo2q4red, {out : {inline : 1}, finalize : exo2q4fin});
+//db.restaurants.mapReduce(function(){emit(this.borough,0);}, function(k,v){return 0;}, {out : {inline : 1}});
+
+/*** EXO 2 : Q5 **************************************************************/
+
+var exo2q5map = function () {
+  /*TODO*/
+}
+
+var exo2q5red = function (key, values) {
+  /*TODO*/
+}
+
+//{
+//	"results" : [
+//		{
+//			"_id" : "Bronx",
+//			"value" : {
+//				"min" : {
+//					"score" : 0,
+//					"id" : "African Terrace"
+//				},
+//				"max" : {
+//					"score" : 82,
+//					"id" : "La Potencia Restaurant"
+//				}
+//			}
+//		},
+//		 ...
+
+
+/*** EXO 2 : Q6 **************************************************************/
+
+exo2q6map = function () {
+  /*TODO*/
+}
+
+
+exo2q6red = function (key, values) {
+  /*TODO*/
+}
+
+exo2q6fin = function (key, val) {
+  /*TODO*/
+};
+
+
+//{
+//	"results" : [
+//		{
+//			"_id" : "Bronx",
+//			"value" : {
+//				"weight" : 2273,
+//				"value" : 24705.066666666677,
+//				"square" : 328788.94833616813,
+//				"avg" : 10.868925062325859,
+//				"std" : 5.150526008810278
+//			}
+//		},
+//		{
+//			"_id" : "Brooklyn",
+//			"value" : {
+//				"weight" : 5894,
+//				"value" : 65394.81706349205,
+//				"square" : 884031.1779332033,
+//				"avg" : 11.09515050279811,
+//				"std" : 5.185607104645353
+//			}
+//		},
+//		{
+//			"_id" : "Manhattan",
+//			"value" : {
+//				"weight" : 9987,
+//				"value" : 110489.30079365098,
+//				"square" : 1487019.4909753744,
+//				"avg" : 11.063312385466205,
+//				"std" : 5.14793996707556
+//			}
+//		},
+//		{
+//			"_id" : "Missing",
+//			"value" : {
+//				"weight" : 50,
+//				"value" : 469,
+//				"square" : 5414,
+//				"avg" : 9.38,
+//				"std" : 4.550801678646008
+//			}
+//		},
+//		{
+//			"_id" : "Queens",
+//			"value" : {
+//				"weight" : 5482,
+//				"value" : 62099.9357142857,
+//				"square" : 846345.8819501137,
+//				"avg" : 11.327970761453065,
+//				"std" : 5.1056995081692245
+//			}
+//		},
+//		{
+//			"_id" : "Staten Island",
+//			"value" : {
+//				"weight" : 935,
+//				"value" : 10457.34642857143,
+//				"square" : 139852.4814753401,
+//				"avg" : 11.18432773109244,
+//				"std" : 4.950946940833734
+//			}
+//		}
+//	],
+//	"timeMillis" : 1233,
+//	"counts" : {
+//		"input" : 25359,
+//		"emit" : 24621,
+//		"reduce" : 1265,
+//		"output" : 6
+//	},
+//	"ok" : 1
+//}
+
+
